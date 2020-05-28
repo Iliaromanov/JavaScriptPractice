a538367384e89fbd531d4c44c99e3f4b04f46475
Iliaromanov <47427592+Iliaromanov@users.noreply.github.com>
2020-05-28 15:41:49 -0400

    Update linearSearch.js

--- algorithms/linearSearch.js
+++ algorithms/linearSearch.js
@@ -1,1 +1,11 @@
 
+// 1. Search a list of ints for a particular integer. Return the index location, -1 if not found.
+
+const findNumber = function(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === target) {
+      return i;
+    }
+  }
+  return -1;
+};
