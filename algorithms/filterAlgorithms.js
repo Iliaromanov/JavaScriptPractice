7de42632d277c59894d8b5cf40afe307347914b7
Iliaromanov <47427592+Iliaromanov@users.noreply.github.com>
2020-05-30 19:11:59 -0400

    Update filterAlgorithms.js

--- algorithms/filterAlgorithms.js
+++ algorithms/filterAlgorithms.js
@@ -1,2 +1,9 @@
 
 // 1. Create a function that will take a list of numbers and return a new list of only the even numbers from the original list.
+
+function filterEvens(nums) {
+  const newList = nums.filter(num => {
+    return num % 2 === 0;
+  });
+  return newList;
+};
