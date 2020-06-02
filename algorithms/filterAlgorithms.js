870fe3c342588c8765e747f4428da941f5792ed6
Iliaromanov <47427592+Iliaromanov@users.noreply.github.com>
2020-06-02 15:27:27 -0400

    Update filterAlgorithms.js

--- algorithms/filterAlgorithms.js
+++ algorithms/filterAlgorithms.js
@@ -24,3 +24,5 @@ const upperVowels = word => {
   return newString;
 };
 
+
+// 3. Create a function that will take a list of names and return a new list of all names that start with a particular character.
