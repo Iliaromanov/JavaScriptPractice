16bea62e66a72460a955fdbc631f44844890c7fd
Iliaromanov <47427592+Iliaromanov@users.noreply.github.com>
2020-06-05 18:15:06 -0400

    Update bubbleSort.js

--- algorithms/bubbleSort.js
+++ algorithms/bubbleSort.js
@@ -1,1 +1,16 @@
 
+// O(n^2)
+const bubbleSort = nums => {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = 0; j < nums.length - i - 1; j++) {
+      let a = nums[j];
+      let b = nums[j+1];
+
+      if (a > b) {
+        nums[j] = b;
+        nums[j+1] = a;
+      };
+    }
+  }
+  return nums;
+};
