b3596912fd0aa75c010954e40342acb8bd84a923
Iliaromanov <47427592+Iliaromanov@users.noreply.github.com>
2020-06-07 20:35:07 -0400

    Update binarySearch.js

--- algorithms/binarySearch.js
+++ algorithms/binarySearch.js
@@ -1,1 +1,18 @@
 
+const binarySearch = (nums, target) => {
+  let left = 0;
+  let right = nums.length;
+
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+    let result = nums[mid];
+
+    if (result === target) {
+      return mid;
+    } else if (result < target) {
+      left = mid;
+    } else if (result > target) {
+      right = mid;
+    };
+  };
+};
